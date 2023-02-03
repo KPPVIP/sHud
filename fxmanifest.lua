aa8f654e1ff330dc246bace818cb37e3183ba461
Guudz <Kilian1702@outlook.com>
2023-02-03 03:17:46 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "adamant"
+
+game "gta5"
+
+client_script "client.lua"
+client_script "watermark.lua"
+
+server_script "server.lua"
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/css/grid.css',
+	'html/js/main.js',
+	'html/img/*',
+	'html/fonts/*'
+}
+
